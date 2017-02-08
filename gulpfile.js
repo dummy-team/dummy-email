78c3e1eca937be9138c1a29606a6393b1b6f5be1
Plou <cl.plou@gmail.com>
2017-02-08 19:06:35 +0100

    feat: improve grid and gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -8,14 +8,19 @@ const sass = require('gulp-sass')
 const inlineCss = require('gulp-inline-css')
 const imagemin = require('gulp-imagemin')
 const sourcemaps = require('gulp-sourcemaps')
+const replace = require('gulp-replace');
 const browserSync = require('browser-sync').create()
 const fs = require('fs')
 const runSequence = require('run-sequence')
+const siphon = require('siphon-media-query');
+
 
 gulp.task('pug', function(){
   return gulp.src('templates/page/*.pug')
     .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
-    .pipe(pug())
+    .pipe(pug({
+      pretty: true
+    }))
     .pipe(gulp.dest('.'))
     .pipe(browserSync.stream())
 })
@@ -32,13 +37,18 @@ gulp.task('scss', function(){
 
 // Inline CSS and minify HTML
 gulp.task('inline', function(){
+  var css = fs.readFileSync('css/main.css').toString();
+  var mqCss = siphon(css);
+
   return gulp.src('./*.html')
     .pipe(inlineCss({
       applyStyleTags: false,
       removeStyleTags: true,
       preserveMediaQueries: true,
-      removeLinkTags: false
+      removeLinkTags: true,
+      applyWidthAttributes: true
     }))
+    .pipe(replace('<!-- <mediaquery> -->', `<style>${mqCss}</style>`))
     .pipe(gulp.dest('dist/'))
 })
 
@@ -59,7 +69,10 @@ gulp.task('serve', function() {
     gulp.watch("css/src/**/*.scss", ['scss'])
     gulp.watch("templates/**/*.pug", ['pug'])
     gulp.watch('img/**/*').on('all', browserSync.reload)
+
+    gulp.watch("./*.html", ['inline'])
+    gulp.watch("css/*.css", ['inline'])
 })
 
-gulp.task('default', [ 'pug', 'scss', 'serve' ])
+gulp.task('default', runSequence([ 'pug', 'scss', 'serve' ], 'inline', 'images'))
 gulp.task('build', runSequence( 'pug', 'scss', 'inline', 'images'))
